2235a11f314f3fd03f5e2368fb80f99390d242e9
techbola <bfagbowore@gmail.com>
2019-08-15 21:46:54 +0100

    mission section completed

--- script.js
+++ script.js
@@ -14,6 +14,18 @@ $(document).ready(function () {
        }
    });
 
+    $(window).scroll(function () {
+        let position = $(this).scrollTop();
+        console.log(position);
+        if (position >= 550){
+            $('.camera-img').addClass('fromLeft');
+            $('.mission-text').addClass('fromRight');
+        } else {
+            $('.camera-img').removeClass('fromLeft');
+            $('.mission-text').removeClass ('fromRight');
+        }
+    });
+
 });
 
 
