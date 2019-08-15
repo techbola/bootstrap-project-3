b372ebd24a08f33666e85f346ed1bd34bfc4f5c9
techbola <bfagbowore@gmail.com>
2019-08-15 22:51:31 +0100

    gallery section completed

--- script.js
+++ script.js
@@ -26,6 +26,30 @@ $(document).ready(function () {
         }
     });
 
+    $('.gallery-list-item').click(function () {
+
+        let value = $(this).attr('data-filter');
+
+        if(value === 'All'){
+
+            $('.filter').show(300);
+
+        } else {
+
+            $('.filter').not('.' + value).hide(300);
+
+            $('.filter').filter('.' + value).show(300);
+
+        }
+
+    });
+
+    $('.gallery-list-item').click(function () {
+
+        $(this).addClass('active-item').siblings().removeClass('active-item');
+
+    });
+
 });
 
 
